1eb76215a4906d15a9e9681e9c74d5749319b082
suvankars <suvankar.17@gmail.com>
2016-04-01 05:41:57 +0530

    fixed issue of deleting subcategory

--- app/controllers/backend/subcategories_controller.rb
+++ app/controllers/backend/subcategories_controller.rb
@@ -1,4 +1,6 @@
 class Backend::SubcategoriesController < BackendController
+  before_action :set_subcategory, only: [:show, :destroy]
+
 	def create
 		@category = Category.find(params[:category_id])
     p subcategory_params
@@ -25,13 +27,24 @@ class Backend::SubcategoriesController < BackendController
     end
   end
 
+  def destroy
+    @subcategory.destroy
+    respond_to do |format|
+      format.html { redirect_to category_path(@category), notice: 'Subcategory was successfully destroyed.' }
+    end
+  end
+
   def show
-    @category = Category.find(params[:category_id])
-    @subcategory = @category.subcategories.find(params[:id])
     @products = Product.where(subcategory_id: params[:id] )
   end
 
 	private
+
+  def set_subcategory
+    @category = Category.find(params[:category_id])
+    @subcategory = @category.subcategories.find(params[:id])
+  end
+
 	def subcategory_params
 		params.require(:subcategory).permit(:name, :description, fields_attributes: [:id, :field_type, :name])	
 	end
--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -1,6 +1,6 @@
 class Subcategory < ActiveRecord::Base
   belongs_to :category
-  has_many :fields, class_name: "ProductField"
+  has_many :fields, class_name: "ProductField", dependent: :destroy
   accepts_nested_attributes_for :fields, allow_destroy: true 
   validates :name, :description, presence: true
   has_many :products, dependent: :destroy
